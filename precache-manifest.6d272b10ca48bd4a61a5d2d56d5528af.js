e52bea81d04758fb5293d880493673625a86548e
Fausto95 <Faustino.kialungila@gmail.com>
2020-01-04 14:36:57 +0000

    Deploying to gh-pages from  922ad6edcbd65727078ff9303aa28a0afe65e8a8

---
+++ precache-manifest.6d272b10ca48bd4a61a5d2d56d5528af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e43d5ed13d5eb58a3d5162e01f92b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a69b6286157749603781",
+    "url": "/static/css/main.e782e69f.chunk.css"
+  },
+  {
+    "revision": "21b9a7a756c5e7b609ca",
+    "url": "/static/js/2.a6731797.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a6731797.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a69b6286157749603781",
+    "url": "/static/js/main.1e4942e5.chunk.js"
+  },
+  {
+    "revision": "b44dee203db29ecae5db",
+    "url": "/static/js/runtime-main.b019aae8.js"
+  }
+]);
